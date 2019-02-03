754c02fdfc19788249905d1394b8550abfbd9d01
Renato Florentino Garcia <fgarcia.renato@gmail.com>
2019-02-03 20:56:17 -0200

    Add version macros.

--- icecream.hpp
+++ icecream.hpp
@@ -30,6 +30,12 @@
 #include <utility>
 
 
+#define ICECREAM_MAJOR_VERSION 0
+#define ICECREAM_MINOR_VERSION 1
+#define ICECREAM_PATCH_VERSION 0
+#define ICECREAM_LIB_VERSION "0.1.0"
+
+
 #if defined(__GNUC__)
     #define ICECREAM_FUNCTION __PRETTY_FUNCTION__
 #elif defined(_MSC_VER)
