45388766a4e4e829ed1410de07ad9162de545469
Renato Garcia <fgarcia.renato@gmail.com>
2026-04-20 22:00:12 -0300

    Migrate from flake-utils to flake-parts
    - Replace flake-utils with flake-parts and nix-systems/default
    - Add nixfmt-rfc-style formatter
    - Update README.md Nix section to document packages.default

--- ci_images/flake.nix
+++ ci_images/flake.nix
@@ -7,68 +7,100 @@
     nixpkgs_23_11.url = "nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs_24_11, nixpkgs_24_05, nixpkgs_23_11 }:
-  let
-    lib = nixpkgs_24_11.lib;
+  outputs =
+    {
+      self,
+      nixpkgs_24_11,
+      nixpkgs_24_05,
+      nixpkgs_23_11,
+    }:
+    let
+      lib = nixpkgs_24_11.lib;
 
-    ci-docker = { pkgs, stdenv, tagname }:
-      pkgs.dockerTools.buildLayeredImage {
-        name = "renatogarcia/icecream-ci";
-        tag = tagname;
+      ci-docker =
+        {
+          pkgs,
+          stdenv,
+          tagname,
+        }:
+        pkgs.dockerTools.buildLayeredImage {
+          name = "renatogarcia/icecream-ci";
+          tag = tagname;
 
-        contents = with pkgs; [
-          busybox
-          cmake
-          conan
-          gnumake
-          stdenv.cc
-        ];
+          contents = with pkgs; [
+            busybox
+            cmake
+            conan
+            gnumake
+            stdenv.cc
+          ];
 
-        # Required by clang using mktemp and failing when /tmp doesn't exist.
-        extraCommands = ''
+          # Required by clang using mktemp and failing when /tmp doesn't exist.
+          extraCommands = ''
             mkdir -p tmp
-        '';
+          '';
 
-        config = {
-          WorkingDir = "/home";
-          Env =
-             # Required so that the linker can find the libc++ when compiling with clang
-            lib.lists.optionals (stdenv.cc.libcxx != null) [
-              "NIX_CC_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu=1"
-              (
-                let
-                  # clang8 depends on libc++abi besides libc++
-                  findLibcxxabi = depsList: lib.lists.findFirst (drv: drv.pname == "libcxxabi") null depsList;
-                  libcxxabi = findLibcxxabi (lib.lists.remove null stdenv.cc.libcxx.buildInputs);
-                in
-                  if libcxxabi != null
-                  then "NIX_LDFLAGS=-L${stdenv.cc.libcxx}/lib/ -L${libcxxabi.out}/lib"
-                  else "NIX_LDFLAGS=-L${stdenv.cc.libcxx}/lib/"
-              )
-            ];
+          config = {
+            WorkingDir = "/home";
+            Env =
+              # Required so that the linker can find the libc++ when compiling with clang
+              lib.lists.optionals (stdenv.cc.libcxx != null) [
+                "NIX_CC_WRAPPER_TARGET_HOST_x86_64_unknown_linux_gnu=1"
+                (
+                  let
+                    # clang8 depends on libc++abi besides libc++
+                    findLibcxxabi = depsList: lib.lists.findFirst (drv: drv.pname == "libcxxabi") null depsList;
+                    libcxxabi = findLibcxxabi (lib.lists.remove null stdenv.cc.libcxx.buildInputs);
+                  in
+                  if libcxxabi != null then
+                    "NIX_LDFLAGS=-L${stdenv.cc.libcxx}/lib/ -L${libcxxabi.out}/lib"
+                  else
+                    "NIX_LDFLAGS=-L${stdenv.cc.libcxx}/lib/"
+                )
+              ];
+          };
         };
+
+      pkgs_24_11 = import nixpkgs_24_11 {
+        system = "x86_64-linux";
       };
 
-    pkgs_24_11 = import nixpkgs_24_11 {
-      system = "x86_64-linux";
-    };
+      pkgs_24_05 = import nixpkgs_24_05 {
+        system = "x86_64-linux";
+      };
 
-    pkgs_24_05 = import nixpkgs_24_05 {
-      system = "x86_64-linux";
-    };
+      pkgs_23_11 = import nixpkgs_23_11 {
+        system = "x86_64-linux";
+      };
 
-    pkgs_23_11 = import nixpkgs_23_11 {
-      system = "x86_64-linux";
+    in
+    {
+      packages.x86_64-linux = {
+        ci-gcc6 = ci-docker {
+          pkgs = pkgs_24_11;
+          stdenv = pkgs_24_05.gcc6Stdenv;
+          tagname = "gcc6-3";
+        };
+        ci-gcc9 = ci-docker {
+          pkgs = pkgs_24_11;
+          stdenv = pkgs_24_11.gcc9Stdenv;
+          tagname = "gcc9-3";
+        };
+        ci-gcc14 = ci-docker {
+          pkgs = pkgs_24_11;
+          stdenv = pkgs_24_11.gcc14Stdenv;
+          tagname = "gcc14-3";
+        };
+        ci-clang8 = ci-docker {
+          pkgs = pkgs_24_11;
+          stdenv = pkgs_23_11.llvmPackages_8.libcxxStdenv;
+          tagname = "clang8-3";
+        };
+        ci-clang19 = ci-docker {
+          pkgs = pkgs_24_11;
+          stdenv = pkgs_24_11.llvmPackages_19.libcxxStdenv;
+          tagname = "clang19-3";
+        };
+      };
     };
-
-  in
-  {
-    packages.x86_64-linux = {
-      ci-gcc6 = ci-docker { pkgs = pkgs_24_11; stdenv = pkgs_24_05.gcc6Stdenv; tagname = "gcc6-3"; };
-      ci-gcc9 = ci-docker { pkgs = pkgs_24_11; stdenv = pkgs_24_11.gcc9Stdenv; tagname = "gcc9-3"; };
-      ci-gcc14 = ci-docker { pkgs = pkgs_24_11; stdenv = pkgs_24_11.gcc14Stdenv; tagname = "gcc14-3"; };
-      ci-clang8 = ci-docker { pkgs = pkgs_24_11; stdenv = pkgs_23_11.llvmPackages_8.libcxxStdenv; tagname = "clang8-3"; };
-      ci-clang19 = ci-docker { pkgs = pkgs_24_11; stdenv = pkgs_24_11.llvmPackages_19.libcxxStdenv; tagname = "clang19-3"; };
-    };
-  };
 }
--- flake.nix
+++ flake.nix
@@ -1,86 +1,107 @@
 {
-  description = "A little library to help with the print debugging on C++11 and forward";
+  description = "A little library to help with print debugging in C++11 and later";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems = {
+      url = "github:nix-systems/default";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  let
-    headerSrc = builtins.readFile ./CMakeLists.txt;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    let
+      cmakeContent = builtins.readFile ./CMakeLists.txt;
 
-    versionNumbersRegex = ".*project[[:space:]]*\\(.*[[:space:]]VERSION[[:space:]]+([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*\\).*";
+      versionNumbersRegex = ".*project[[:space:]]*\\(.*[[:space:]]VERSION[[:space:]]+([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*\\).*";
 
-    versionNumbers = builtins.head (builtins.match versionNumbersRegex headerSrc);
+      versionNumbers = builtins.head (builtins.match versionNumbersRegex cmakeContent);
 
-    devHash_ = builtins.match ".*DEV_HASH: ([[:xdigit:]]{8}).*" headerSrc;
-    devHash = if devHash_ != null then builtins.head devHash_ else null;
+      devHash_ = builtins.match ".*DEV_HASH: ([[:xdigit:]]{8}).*" cmakeContent;
+      devHash = if devHash_ != null then builtins.head devHash_ else null;
 
-    version =
-      versionNumbers +
-      (
-        if devHash != null
-        then "+dev." + devHash
-        else "+local_repo"
-      );
+      version = versionNumbers + (if devHash != null then "+dev." + devHash else "+local_repo");
 
-    application = { pkgs }:
-      pkgs.stdenv.mkDerivation {
-        pname = "icecream-cpp";
-        inherit version;
+      library =
+        { pkgs }:
+        pkgs.stdenv.mkDerivation {
+          pname = "icecream-cpp";
+          inherit version;
 
-        src = ./.;
+          src = ./.;
 
-        nativeBuildInputs = [ pkgs.buildPackages.cmake ];
+          nativeBuildInputs = [ pkgs.buildPackages.cmake ];
 
-        meta = with pkgs.lib; {
-          homepage = "https://github.com/renatoGarcia/icecream-cpp";
-          description = "A little library to help with the print debugging on C++11 and forward";
-          license = licenses.mit;
-          maintainers = with maintainers; [ renatoGarcia ];
+          meta = with pkgs.lib; {
+            homepage = "https://github.com/renatoGarcia/icecream-cpp";
+            description = "A little library to help with print debugging in C++11 and later";
+            license = licenses.mit;
+            maintainers = with maintainers; [ renatoGarcia ];
+          };
         };
-      };
 
-      dev-env = { pkgs, stdenv }:
+      dev-env =
+        { pkgs, stdenv }:
         let
-          app = application { inherit pkgs; };
+          package = library { inherit pkgs; };
         in
-          pkgs.mkShell.override { inherit stdenv; } {
-            name = "icecream-cpp";
-            nativeBuildInputs = with pkgs.buildPackages; [
+        pkgs.mkShell.override { inherit stdenv; } {
+          name = "icecream-cpp";
+          nativeBuildInputs =
+            with pkgs.buildPackages;
+            [
               (fmt.override { inherit stdenv; })
               boost
               clang-tools
               cmakeCurses
               conan
               range-v3
-            ] ++ app.nativeBuildInputs;
-            hardeningDisable = [ "fortify" ];
-       };
+            ]
+            ++ package.nativeBuildInputs;
+          hardeningDisable = [ "fortify" ];
+        };
 
-    in {
-      overlays.default = final: prev: {
-        icecream-cpp = application { pkgs = final; };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      flake = {
+        overlays.default = final: prev: {
+          icecream-cpp = library { pkgs = final; };
+        };
       };
-    }
-    //
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          packages.default = pkgs.icecream-cpp;
+
+          devShells = rec {
+            gcc = dev-env {
+              inherit pkgs;
+              stdenv = pkgs.stdenv;
+            };
+            clang = dev-env {
+              inherit pkgs;
+              stdenv = pkgs.libcxxStdenv;
+            };
+            default = gcc;
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
         };
-      in
-      {
-        packages = {
-          default = pkgs.icecream-cpp;
-        };
-        devShells = rec {
-          gcc = dev-env { inherit pkgs; stdenv = pkgs.stdenv; };
-          clang = dev-env { inherit pkgs; stdenv = pkgs.libcxxStdenv; };
-          default = gcc;
-        };
-      }
-    );
+    };
 }
